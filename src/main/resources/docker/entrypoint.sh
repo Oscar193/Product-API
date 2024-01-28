654c9cd3557b1d1c0295ca8e2a10a73ef92a6a4a
oscar.lopez <oscar.lopez@enverus.com>
2024-01-28 12:41:18 +0100

    PS-1 Refactor.

--- src/main/resources/docker/entrypoint.sh
+++ src/main/resources/docker/entrypoint.sh
@@ -1,23 +1,4 @@
 #!/bin/sh
-#
-## Set JVM max memory to MAX_MEMORY_PERCENTAGE of the available memory, if not already set in JAVA_OPTS
-#set_jvm_max_memory() {
-#  if test -z "${MAX_MEMORY_PERCENTAGE}"; then
-#    return
-#  fi
-#
-#  maxMemoryFlag='-XX:MaxRAMPercentage'
-#  if test "${JAVA_OPTS#*$maxMemoryFlag}" != "${JAVA_OPTS}"; then
-#    # JVM max memory already set
-#    return
-#  fi
-#
-#  export JAVA_OPTS="${maxMemoryFlag}=${MAX_MEMORY_PERCENTAGE} ${JAVA_OPTS}"
-#
-#  echo "entrypoint: Setting max memory: ${JAVA_OPTS}"
-#}
-#
-#set_jvm_max_memory
 
 if [ -n "${APPLICATIONINSIGHTS_CONNECTION_STRING}" ]; then
   export JAVA_OPTS="${APPLICATIONINSIGHTS_OPTS} ${JAVA_OPTS}"
